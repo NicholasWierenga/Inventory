b4b5bba39f5ce8c9630db063f7b9e25fce37a517
Nicholas Wierenga <NicholasWierenga@gmail.com>
2022-06-15 16:10:05 -0400

    initial commit - nothing done yet

---
+++ Inventory/Models/KrogerDal.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace GC_Cars_Web_App.Models
+{
+    public class KrogerDal
+    {
+        public string CallAPI(string endpoint)
+        {
+            string url = "https://localhost:7103/GC_Dealership/" + endpoint;
+            Console.WriteLine(url);
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+
+            string JSON = rd.ReadToEnd();
+
+            return JSON;
+        }
+
+        public List<Car> GetCars()
+        {
+            string result = CallAPI("AllCars");
+
+            List<Car> cars = JsonConvert.DeserializeObject<List<Car>>(result);
+
+            return cars;
+        }
+
+        public List<Car> SearchForCars(string Make, string Model, int Year, string Color)
+        {
+            string result = CallAPI($"SearchForCar/{Make}&{Model}&{Year}&{Color}");
+
+            List<Car> cars = JsonConvert.DeserializeObject<List<Car>>(result);
+
+            return cars;
+        }
+    }
+}
