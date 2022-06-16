d776abb7a2ee0b8100a3ac7e8481c5252d7a5c7e
Seth Adams <sadams@wwiflint.com>
2022-06-16 14:51:22 -0400

    Checking our Azure connections

---
+++ Inventory/Models/InventoryContext.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Inventory.Models
+{
+    public partial class InventoryContext : DbContext
+    {
+        public InventoryContext()
+        {
+        }
+
+        public InventoryContext(DbContextOptions<InventoryContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Order> Orders { get; set; } = null!;
+        public virtual DbSet<ProductInv> Products { get; set; } = null!;
+        public virtual DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=tcp:gcinventoryfinal.database.windows.net,1433;Initial Catalog=Inventory;Persist Security Info=False;User ID=GCFinal;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity.Property(e => e.OrderDate).HasColumnType("date");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.Orders)
+                    .HasForeignKey(d => d.UserId)
+                    .HasConstraintName("FK__Orders__UserId__38996AB5");
+            });
+
+            modelBuilder.Entity<ProductInv>(entity =>
+            {
+                entity.ToTable("Product");
+
+                entity.Property(e => e.Description).HasMaxLength(80);
+
+                entity.Property(e => e.ProductName).HasMaxLength(40);
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("User");
+
+                entity.Property(e => e.Address).HasMaxLength(60);
+
+                entity.Property(e => e.Email).HasMaxLength(30);
+
+                entity.Property(e => e.FirstName).HasMaxLength(25);
+
+                entity.Property(e => e.LastName).HasMaxLength(25);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
